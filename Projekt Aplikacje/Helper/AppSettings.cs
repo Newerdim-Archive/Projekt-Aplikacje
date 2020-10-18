79ad450c83e0e101a253c05dbf791fb03981457c
Newerdim <newerdim@onet.pl>
2020-10-18 12:05:19 +0200

    Remove default controller and model. Add appsettings class

---
+++ Projekt Aplikacje/Helper/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_Aplikacje.Helper
+{
+    public class AppSettings
+    {
+        public string AccessTokenSecret { get; set; }
+        public string RefreshTokenSecret { get; set; }
+        public string ForgotPasswordTokenSecret { get; set; }
+    }
+}
