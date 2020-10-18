1f0baa866e0bedc715bac1235454bdda352334e5
Newerdim <newerdim@onet.pl>
2020-10-18 12:03:43 +0200

    Add user dtos

---
+++ Projekt Aplikacje/Dtos/User/RecoveryPasswordDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Dto.User
+{
+    public class RecoveryPasswordDto
+    {
+        [Required]
+        public string NewPassword { get; set; }
+    }
+}
---
+++ Projekt Aplikacje/Dtos/User/UserForChangePasswordDto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Dto
+{
+    public class UserForChangePasswordDto
+    {
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string NewPassword { get; set; }
+    }
+}
