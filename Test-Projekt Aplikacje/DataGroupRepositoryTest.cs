30a1aca35650bb372d0b351d4ce24086198e238b
Newerdim <newerdim@onet.pl>
2020-10-21 13:42:39 +0200

    Add tests for data and datagroup repos

---
+++ Test-Projekt Aplikacje/DataGroupRepositoryTest.cs
@@ -0,0 +1,72 @@
+﻿using Projekt_Aplikacje.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Todo.Data;
+using Xunit;
+
+namespace Test_Projekt_Aplikacje
+{
+    public class DataGroupRepositoryTest : IClassFixture<DataSeedFixture>
+    {
+        private readonly DataSeedFixture _fixture;
+        private readonly DataContext _context;
+        private readonly IUnitOfWork _uow;
+
+        public DataGroupRepositoryTest(DataSeedFixture fixture)
+        {
+            _fixture = fixture;
+            _context = fixture.DataContext;
+            _uow = new UnitOfWork(_context);
+        }
+
+        [Theory]
+        [InlineData(999, 2)]
+        public async void GetByIdWithUserDataAsync_UserDoesNotExists(int userId, int dataGroupId)
+        {
+            var dataGroup = await _uow.DataGroup.GetByIdWithUserDataAsync(dataGroupId, userId);
+            Assert.False(dataGroup.Datas.Any());
+        }
+
+        [Theory]
+        [InlineData(2, 999)]
+        public async void GetByIdWithUserDataAsync_DataGroupDoesNotExists(int userId, int dataGroupId)
+        {
+            var dataGroup = await _uow.DataGroup.GetByIdWithUserDataAsync(dataGroupId, userId);
+            Assert.Null(dataGroup);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 1)]
+        public async void GetByIdWithUserDataAsync_Valid(int userId, int dataGroupId, int expected)
+        {
+            var dataGroup = await _uow.DataGroup.GetByIdWithUserDataAsync(dataGroupId, userId);
+            Assert.Equal(expected, dataGroup.Datas.Count);
+        }
+
+        [Theory]
+        [InlineData(999, "Waga")]
+        public async void GetByNameWithUserDataAsync_UserDoesNotExists(int userId, string dataGroupName)
+        {
+            var dataGroup = await _uow.DataGroup.GetByNameWithUserDataAsync(dataGroupName, userId);
+            Assert.False(dataGroup.Datas.Any());
+        }
+
+        [Theory]
+        [InlineData(2, "NotFound")]
+        public async void GetByNameWithUserDataAsync_DataGroupDoesNotExists(int userId, string dataGroupName)
+        {
+            var dataGroup = await _uow.DataGroup.GetByNameWithUserDataAsync(dataGroupName, userId);
+            Assert.Null(dataGroup);
+        }
+
+        [Theory]
+        [InlineData(1, "Waga", 1)]
+        public async void GetByNameWithUserDataAsync_Valid(int userId, string dataGroupName, int expected)
+        {
+            var dataGroup = await _uow.DataGroup.GetByNameWithUserDataAsync(dataGroupName, userId);
+            Assert.Equal(expected, dataGroup.Datas.Count);
+        }
+    }
+}
---
+++ Test-Projekt Aplikacje/DataSeedFixture.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projekt_Aplikacje.Data;
+using Projekt_Aplikacje.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test_Projekt_Aplikacje
+{
+    public class DataSeedFixture : IDisposable
+    {
+        public DataContext DataContext { get; private set; }
+
+        public DataSeedFixture()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase("DataGroupDatabase")
+            .Options;
+
+            DataContext = new DataContext(options);
+
+            #region Seed Data
+            var dataGroups = new List<DataGroup>
+            {
+                new DataGroup { Name = "Waga", Unit = "kg" }, // Id 1
+                new DataGroup { Name = "Kalorie", Unit = "kcal" }, // Id 2
+                new DataGroup { Name = "Sen", Unit = "h" }, // Id 3
+                new DataGroup { Name = "Woda", Unit = "ml" }, // Id 4
+                new DataGroup { Name = "Trening", Unit = "h" } // Id 5
+            };
+
+            var datas = new List<DataModel>
+            {
+                new DataModel { DataGroupId = 1, Date = DateTime.Now, UserId = 1, Value = 80 },
+                new DataModel { DataGroupId = 5, Date = DateTime.Now, UserId = 1, Value = 1.5 },
+                new DataModel { DataGroupId = 3, Date = DateTime.Now, UserId = 1, Value = 8 },
+                new DataModel { DataGroupId = 1, Date = DateTime.Now, UserId = 2, Value = 82 },
+                new DataModel { DataGroupId = 2, Date = DateTime.Now, UserId = 2, Value = 1500 },
+                new DataModel { DataGroupId = 2, Date = DateTime.Now, UserId = 3, Value = 2550 },
+                new DataModel { DataGroupId = 4, Date = DateTime.Now, UserId = 3, Value = 3000 },
+            };
+            #endregion
+
+            DataContext.DataGroups.AddRange(dataGroups);
+            DataContext.Datas.AddRange(datas);
+            DataContext.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            DataContext.Dispose();
+        }
+    }
+}
