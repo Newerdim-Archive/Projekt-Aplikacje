febf5f06fdb3c2b0d19e30bb8d2bce6f629083f2
Newerdim <newerdim@onet.pl>
2020-10-21 16:13:59 +0200

    Add data dtos

---
+++ Projekt Aplikacje/Dtos/Data/DataForUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projekt_Aplikacje.Dtos.Data
+{
+    public class DataForUpdateDto
+    {
+        public double Value { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public int DataGroupId { get; set; }
+    }
+}
