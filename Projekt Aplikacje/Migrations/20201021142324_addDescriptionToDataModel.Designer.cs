7bcff30ae73523fd49c686bea544c3613e9ed740
Newerdim <newerdim@onet.pl>
2020-10-21 16:25:35 +0200

    Update packages and add migration

--- Projekt Aplikacje/Migrations/20201021142324_addDescriptionToDataModel.Designer.cs
+++ Projekt Aplikacje/Migrations/20201021142324_addDescriptionToDataModel.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Projekt_Aplikacje.Data;
 
 namespace Projekt_Aplikacje.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20201021142324_addDescriptionToDataModel")]
+    partial class addDescriptionToDataModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Projekt Aplikacje/Migrations/20201021142324_addDescriptionToDataModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Projekt_Aplikacje.Migrations
+{
+    public partial class addDescriptionToDataModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Datas",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "DataGroups",
+                columns: new[] { "Id", "Name", "Unit" },
+                values: new object[] { 6, "Samopoczucie", "" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "DataGroups",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Datas");
+        }
+    }
+}
