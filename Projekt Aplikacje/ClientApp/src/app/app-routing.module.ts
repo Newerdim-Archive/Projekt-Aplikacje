3e524b4e8de5580049036be5438aa2ff94a30818
Newerdim <newerdim@onet.pl>
2020-10-24 15:20:05 +0200

    Add auth guard and change login route

--- Projekt Aplikacje/ClientApp/src/app/app-routing.module.ts
+++ Projekt Aplikacje/ClientApp/src/app/app-routing.module.ts
@@ -1,15 +1,14 @@
+import { AuthGuard } from './guards/auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DataSummaryComponent } from './components/data-summary/data-summary.component';
 import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { UserFormsComponent } from './components/user-forms/user-forms.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'data', component: DataSummaryComponent, pathMatch: 'full' },
-  { path: 'zaloguj', component: LoginComponent, pathMatch: 'full' },
-  { path: 'zarejestruj', component: RegisterComponent, pathMatch: 'full' },
+  { path: 'dane/:dataName', component: DataSummaryComponent, pathMatch: 'full', canActivate: [AuthGuard] },
+  { path: 'logowanie', component: UserFormsComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
