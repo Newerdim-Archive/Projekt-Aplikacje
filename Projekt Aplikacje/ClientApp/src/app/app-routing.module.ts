dd4c2a61c63e40b8a5fbb593759afb65012c52b4
Newerdim <newerdim@onet.pl>
2020-10-25 19:05:18 +0100

    Add kalkulator and error route

--- Projekt Aplikacje/ClientApp/src/app/app-routing.module.ts
+++ Projekt Aplikacje/ClientApp/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { ErrorComponent } from './components/error/error.component';
+import { CalculatorsComponent } from './components/calculators/calculators.component';
 import { AuthGuard } from './guards/auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -9,6 +11,8 @@ const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'dane/:dataName', component: DataGroupComponent, pathMatch: 'full', canActivate: [AuthGuard] },
   { path: 'logowanie', component: UserFormsComponent, pathMatch: 'full' },
+  { path: 'kalkulatory', component: CalculatorsComponent, pathMatch: 'full'},
+  { path: 'error', component: ErrorComponent, pathMatch: 'full'}
 ];
 
 @NgModule({
