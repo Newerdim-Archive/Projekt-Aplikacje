4b769eefaab8c290970fb980481d05871b05a8cb
Newerdim <newerdim@onet.pl>
2020-10-25 19:07:27 +0100

    Add home component

--- Projekt Aplikacje/ClientApp/src/app/components/home/home.component.ts
+++ Projekt Aplikacje/ClientApp/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,39 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  quotes = [
-    'Krytykować, potępiać i narzekać potrafi każdy głupiec – i większość głupców tak robi. Ale aby zrozumieć i darować, potrzeba charakteru i samokontroli.',
-    'Początek jest najważniejszą częścią pracy'
-  ];
+  constructor(private authService: AuthService) { }
 
-  months = [
-    'Styczeń',
-    'Luty',
-    'Marzec',
-    'Kwiecien',
-    'Maj',
-    'Czerwiec',
-    'Lipiec',
-    'Sierpień',
-    'Wrzesień',
-    'Paździenik',
-    'Listopad',
-    'Grudzień'
-  ]
+  ngOnInit(): void {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
   }
-
-  todayDate() {
-    let date = new Date();
-    return `${date.getDate()} ${this.months[date.getMonth()]}`;
-  }
-
-  randomQuote() {
-    return this.quotes[Math.floor(Math.random() * (this.quotes.length - 1))];
-  }
-
 }
